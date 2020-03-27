11b6ca3c821d1123672ddd60a884a0cd782c703a
Josef Rousek <josef.rousek@gmail.com>
2020-03-27 13:54:10 +0100

    Add Shopify rate limit test

--- test_client.py
+++ test_client.py
@@ -1,13 +1,30 @@
 import unittest
 from pyactiveresource.connection import Connection
+import shopify
+from concurrent.futures import ProcessPoolExecutor
 
-# https://github.com/Shopify/pyactiveresource/blob/master/pyactiveresource/connection.py#L304
+import django_toolbox.shopify_limits_patch
+
+def get_shop(item):
+    print(item)
+    session = shopify.Session(".myshopify.com", "2020-01", "TOKEN")
+    shopify.ShopifyResource.activate_session(session)
+    return shopify.Shop.current()
+
 class ClientTest(unittest.TestCase):
 
-    def test_success(self):
+    def test_httpbin(self):
         connection = Connection("https://httpbin.org")
         response = connection.get("/delay/5")
         print(response)
+
+    def test_shopify_rate_limit(self):
+        """
+        Hit Shopify rate limiting by requesting shop resource 50 times in batches of 10 requests at once.
+        """
+        pool = ProcessPoolExecutor(max_workers=10)
+        print(list(pool.map(get_shop, range(50))))
+
 
 if __name__ == "__main__":
     unittest.main()
